afa91acb10d54e849d459de344eff3e87bac4812
Frederick Garner Wibowo <frederickgarner06@gmail.com>
2025-11-17 23:55:44 +0700

    Commit No. 3: Integrate Room Database for Data Persistence

--- app/src/main/java/com/fredcodecrafts/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/fredcodecrafts/lab_week_10/MainActivity.kt
@@ -1,21 +1,35 @@
 package com.fredcodecrafts.lab_week_10
 
-
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.fredcodecrafts.lab_week_10.database.Total
+import com.fredcodecrafts.lab_week_10.database.TotalDatabase
 import com.fredcodecrafts.lab_week_10.viewmodels.TotalViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private val db by lazy { prepareDatabase() }
     private val viewModel by lazy { ViewModelProvider(this)[TotalViewModel::class.java] }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeValueFromDatabase()
         prepareViewModel()
+    }
+
+    private fun prepareDatabase(): TotalDatabase = Room.databaseBuilder(
+        applicationContext, TotalDatabase::class.java, "total-database"
+    ).allowMainThreadQueries().build()
+
+    private fun initializeValueFromDatabase() {
+        val totalList = db.totalDao().getTotal(ID)
+        if (totalList.isEmpty()) db.totalDao().insert(Total(id = 1, total = 0))
+        else viewModel.setTotal(totalList.first().total)
     }
 
     private fun updateText(total: Int) {
@@ -24,12 +38,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun prepareViewModel() {
-        viewModel.total.observe(this) { total ->
-            updateText(total)
-        }
-
-        findViewById<Button>(R.id.button_increment).setOnClickListener {
-            viewModel.incrementTotal()
-        }
+        viewModel.total.observe(this) { total -> updateText(total) }
+        findViewById<Button>(R.id.button_increment).setOnClickListener { viewModel.incrementTotal() }
     }
+
+    override fun onPause() {
+        super.onPause()
+        db.totalDao().update(Total(ID, viewModel.total.value!!))
+    }
+
+    companion object { const val ID: Long = 1 }
 }
